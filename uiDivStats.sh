3cbfd824c1ad5b41c3ec78d07815cf015bd2a040
Jack <jack.naisbett@gmail.com>
2019-05-01 22:49:14 +0100

    Move labels on bars down

--- uiDivStats.sh
+++ uiDivStats.sh
@@ -693,7 +693,7 @@ echo 'set xtics rotate'
 echo 'set border lc rgb "white"'
 echo 'set xtics textcolor rgb "white"'
 echo 'set ytics textcolor rgb "white"'
-echo 'plot "'"$1"'" using 0:1:xtic(2) notitle with boxes lc rgb "white" , "'"$1"'" using 0:($1-25):1 notitle with labels lc rgb "black"'; } > /tmp/gnuplot.script
+echo 'plot "'"$1"'" using 0:1:xtic(2) notitle with boxes lc rgb "white" , "'"$1"'" using 0:($1-35):1 notitle with labels lc rgb "black"'; } > /tmp/gnuplot.script
 	gnuplot /tmp/gnuplot.script
 	#cp "$1" /tmp/bak.dat
 	rm -f /tmp/gnuplot.script
