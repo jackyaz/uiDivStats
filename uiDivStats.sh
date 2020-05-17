dc7237735b1204c6c4478abbbc3ae32c9ef728a3
Jack <jack.naisbett@gmail.com>
2020-05-17 21:37:46 +0100

    Fix branch switching commands

--- uiDivStats.sh
+++ uiDivStats.sh
@@ -1150,14 +1150,14 @@ case "$1" in
 	;;
 	develop)
 		Check_Lock
-		sed -i 's/^readonly SCRIPT_BRANCH.*$/readonly SCRIPT_BRANCH="develop"/' "/jffs/scripts/$SCRIPT_NAME_LOWER"
+		sed -i 's/^readonly SCRIPT_BRANCH.*$/readonly SCRIPT_BRANCH="develop"/' "/jffs/scripts/$SCRIPT_NAME"
 		Clear_Lock
 		exec "$0" "update"
 		exit 0
 	;;
 	stable)
 		Check_Lock
-		sed -i 's/^readonly SCRIPT_BRANCH.*$/readonly SCRIPT_BRANCH="master"/' "/jffs/scripts/$SCRIPT_NAME_LOWER"
+		sed -i 's/^readonly SCRIPT_BRANCH.*$/readonly SCRIPT_BRANCH="master"/' "/jffs/scripts/$SCRIPT_NAME"
 		Clear_Lock
 		exec "$0" "update"
 		exit 0
