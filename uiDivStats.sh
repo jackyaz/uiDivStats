1b7d59b75f54ac9cfd92767b200121a404be1a9d
Jack <jack.naisbett@gmail.com>
2020-06-08 18:51:39 +0100

    Set branch

--- uiDivStats.sh
+++ uiDivStats.sh
@@ -16,7 +16,7 @@
 ### Start of script variables ###
 readonly SCRIPT_NAME="uiDivStats"
 readonly SCRIPT_VERSION="v2.0.1"
-readonly SCRIPT_BRANCH="develop"
+readonly SCRIPT_BRANCH="cachetotmp"
 readonly SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/""$SCRIPT_NAME""/""$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME.d"
 readonly SCRIPT_WEBPAGE_DIR="$(readlink /www/user)"
