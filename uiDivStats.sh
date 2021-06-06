652a5370f63091ddbe53d9f62f5c6593f98a2291
Jack Naisbett <jack.naisbett@gmail.com>
2021-06-06 23:11:01 +0100

    Add delays to installer

--- uiDivStats.sh
+++ uiDivStats.sh
@@ -1836,10 +1836,14 @@ Menu_Install(){
 	/opt/etc/init.d/S90taildns start >/dev/null 2>&1
 	
 	nslookup snbforums.com 127.0.0.1 >/dev/null 2>&1
+	sleep 1
 	nslookup diversion-adblocking-ip.address 127.0.0.1 >/dev/null 2>&1
+	sleep 1
 	
 	Flush_Cache_To_DB
+	sleep 1
 	Generate_Query_Log
+	sleep 1
 	
 	Menu_GenerateStats fullrefresh
 	
