be6d88536f19a60c0e82b3335cbbb774cbbce81b
Jack Naisbett <jack.naisbett@gmail.com>
2021-08-05 19:10:32 +0100

    Version bump

--- uiDivStats.sh
+++ uiDivStats.sh
@@ -26,7 +26,7 @@
 
 ### Start of script variables ###
 readonly SCRIPT_NAME="uiDivStats"
-readonly SCRIPT_VERSION="v3.0.0"
+readonly SCRIPT_VERSION="v3.0.1"
 SCRIPT_BRANCH="master"
 SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME.d"
